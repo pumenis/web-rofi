3b8ff00d257736250a8b69e5b7264d5e23363102
Mindia Edisherashvili <mindia.e@aol.com>
2025-08-27 15:09:03 +0400

    increased font size inside bible display

--- example-app-installer/cgi-bin/display.sh
+++ example-app-installer/cgi-bin/display.sh
@@ -9,6 +9,7 @@ echo '<!doctype html>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title></title>
+    <style>:root{font-size:1.3rem;}</style>
   </head>
   <body>'
 
